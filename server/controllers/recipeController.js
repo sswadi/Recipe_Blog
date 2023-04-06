b2e48472322e79a542daff5d5e23200067403090
swati <s.swati.5293@gmail.com>
2023-04-06 23:53:22 +0530

    6th Apr'23

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -45,6 +45,25 @@ exports.exploreCategories = async(req, res) => {
 }
 
 
+//exploreCategoriesById route ---> GET /categories/:id
+
+exports.exploreCategoriesById = async(req, res) => {
+    try{ 
+        
+        let categoryId = req.params.id;
+        const limitNumber = 20;
+        const categoryById = await Category.find({'category': categoryId}).limit(limitNumber);
+        res.render('categories', {
+            title : 'Cooking Blog - Categories',
+            categoryById
+        });
+
+    }catch(error){
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
+}
+
+
 // Individual recipe page --> GET /recipe/:id
 exports.exploreRecipe = async(req, res) => {
     try{   
@@ -60,6 +79,22 @@ exports.exploreRecipe = async(req, res) => {
         res.status(500).send({message: error.message || "Error Occured"});
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // async function insertDummyCategoryData(){
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -5,7 +5,9 @@ const recipeController = require('../controllers/recipeController');
 // App routes
 
 router.get('/', recipeController.homepage);
-router.get('/categories', recipeController.exploreCategories);
 router.get('/recipe/:id', recipeController.exploreRecipe);
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+
 
 module.exports = router;
