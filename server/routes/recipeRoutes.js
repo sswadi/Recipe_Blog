0ca67848c66af38640a4f1d68d74808e0f73ddbb
swati <s.swati.5293@gmail.com>
2023-04-09 00:24:59 +0530

    9th Apr'23

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', recipeController.homepage);
 router.get('/recipe/:id', recipeController.exploreRecipe);
 router.get('/categories', recipeController.exploreCategories);
 router.get('/categories/:id', recipeController.exploreCategoriesById);
-router.post('/search', recipeController.searchRecipe);s
+router.post('/search', recipeController.searchRecipe);
 router.get('/explore-latest', recipeController.exploreLatest);
 router.get('/explore-random', recipeController.exploreRandom);
 router.get('/submit-recipe', recipeController.submitRecipe);
